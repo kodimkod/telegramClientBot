38287730ad41158f0e7655b60adf1f45d09847fd
Bot Author <nomail@nospam.com>
2018-12-27 23:26:16 +0100

    update composer

---
+++ update.cmd
@@ -0,0 +1,9 @@
+set COMPOSER_VERSION=download/1.3.0/
+set GIT_SSH=c:\Program Files (x86)\putty\plink.exe
+set PHPBIN=c:\xampp\php\php.exe
+powershell -command "& { iwr https://getcomposer.org/%COMPOSER_VERSION%composer.phar -OutFile .\composer.phar }"
+%PHPBIN% .\composer.phar self-update --stable
+%PHPBIN% .\composer.phar update
+%PHPBIN% .\composer.phar install
+%PHPBIN% .\composer.phar dumpautoload -o
+del .\composer.phar
